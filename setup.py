c5856e2cc8a8916df711961e276131e3a661c7f6
tsu <tsu@yaroot.net>
2018-04-18 08:55:29 +0800

    set version from TRAVIS_TAG

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+import os
 from setuptools import setup
+
+_version = os.environ.get('TRAVIS_TAG', '0.0.0')
 
 setup(
     name='semaphore-deploy',
-    version='0.1.0',
+    version=_version,
     packages=['semaphore_deploy'],
     # scripts=['semaphore'],
 )
